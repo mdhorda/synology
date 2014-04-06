970cfa24a502280c0ce3d05824499bdccf9e516b
satreix <satreix@gmail.com>
2014-04-06 02:14:49 +0200

    Added setup.py and __init__.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='synology',
+      version='0.1',
+      description='Synology DSM 5 API',
+      url='http://github.com/satreix/synology',
+      author='Steve Barrau',
+      author_email='satreix@gmail.com',
+      license='MIT',
+      packages=['synology'],
+      install_requires=[
+          'urllib3',
+          'clint'
+      ],
+      zip_safe=False)
